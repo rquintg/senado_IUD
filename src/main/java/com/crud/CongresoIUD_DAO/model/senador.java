5debb1c92c4cd5502341acf723fcb405c705d49c
rquintg <ricardoqg41@icloud.com>
2023-07-11 22:52:11 -0500

    Se acutaliza Modelo

--- src/main/java/com/crud/CongresoIUD_DAO/model/senador.java
+++ src/main/java/com/crud/CongresoIUD_DAO/model/senador.java
@@ -17,11 +17,14 @@ public class senador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String nombre;
 
-    private String partido;
+
+    @Column(name = "departamento")
     private String departamento;
+
+    @Column(name = "fecha")
     private LocalDate fecha;
 
     @ManyToOne
@@ -30,58 +33,5 @@ public class senador {
 
     public senador(){}
 
-   /* public senador(long id, String nombre, String partido, String departamento, LocalDate fecha) {
-        this.id = id;
-        this.nombre = nombre;
-        this.partido = partido;
-        this.departamento = departamento;
-        this.fecha = fecha;
-    }
 
-    public senador(String nombre, String partido, String departamento, LocalDate fecha) {
-        this.nombre = nombre;
-        this.partido = partido;
-        this.departamento = departamento;
-        this.fecha = fecha;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getPartido() {
-        return partido;
-    }
-
-    public void setPartido(String partido) {
-        this.partido = partido;
-    }
-
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }*/
 }
